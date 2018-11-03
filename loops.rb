bf733e4bbdb5d1f37bb894911fe173217ca454ff
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-03 21:47:19 +0000

    automatically backed up by learn

--- loops.rb
+++ loops.rb
@@ -1,5 +1,5 @@
 def loop_iterator(number_of_times)
-    loop do
+  loop do
     number_of_times = number_of_times + 1
     puts "Welcome to Flatiron School's Web Development Course!"
     if number_of_times >= number_of_times
