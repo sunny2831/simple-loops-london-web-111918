361ed58fe21e1c2e8b2ece4741603302af345121
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-03 21:55:36 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -3,9 +3,9 @@
 def loop_iterator(number_of_times)
   counter = 0
   loop do
-        puts "Welcome to Flatiron School's Web Development Course!"
-        counter += 1
-    if number_of_times == number_of_times
+    puts "Welcome to Flatiron School's Web Development Course!"
+    counter += 1
+    if counter == number_of_times
       break
     end
   end
