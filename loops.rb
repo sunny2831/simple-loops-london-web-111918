9975d994bcdcc96491c1285d6f8104191a66c8e6
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-03 21:35:19 +0000

    automatically backed up by learn

---
+++ loops.rb
@@ -0,0 +1,10 @@
+def loop_iterator(number_of_times)
+  number_of_times = 0
+  loop do
+    number_of_times = number_of_times + 1
+    puts "Welcome to Flatiron School's Web Development Course!"
+    if number_of_times >= 7
+      break
+    end
+  end
+end
